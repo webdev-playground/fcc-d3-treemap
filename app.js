9818c165002a0bb06b1f53915d14a34b811a964e
Josh Choo <dev@joshuous.com>
2020-02-10 19:44:56 +0800

    draw legend

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
-const width = 1000;
-const height = 800;
+const width = 800;
+const height = 600;
 const margin = 10;
 
 const svg = d3
   .select('#map')
   .append('svg')
   .attr('width', width + margin)
-  .attr('height', height + margin);
+  .attr('height', height + margin + 200);
 
 d3.json(
   'https://cdn.freecodecamp.org/testable-projects-fcc/data/tree_map/movie-data.json'
@@ -14,13 +14,17 @@ d3.json(
   .then(movies => {
     const numCategories = movies.length;
     const categoryColors = d3.schemePastel2;
+    const categories = movies.children.map(elem => elem.name);
 
     const root = d3.hierarchy(movies).sum(function(d) {
       return parseInt(d.value) || 0;
     });
 
     // prepare color scale
-    const colorScale = d3.scaleOrdinal(categoryColors);
+    const colorScale = d3
+      .scaleOrdinal()
+      .domain(categories)
+      .range(categoryColors);
 
     // prepare data for treemap
     d3
@@ -51,5 +55,17 @@ d3.json(
       .attr('data-name', d => d.data.name)
       .attr('data-category', d => d.data.category)
       .attr('data-value', d => d.data.value);
+
+    // Draw Legend
+    const colorLegend = d3.legendColor().scale(colorScale);
+    svg
+      .append('g')
+      .attr('id', 'legend')
+      .attr('transform', `translate(${width / 2},${height + 20})`);
+    svg.select('#legend').call(colorLegend);
+    svg
+      .select('#legend')
+      .selectAll('rect')
+      .attr('class', 'legend-item');
   })
   .catch(() => alert('An error occurred!'));
